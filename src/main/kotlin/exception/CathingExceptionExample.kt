253abfc3a61e579d9601c2ca9c0fd717bc01f27a
Pushpak <pushpak.gosavi7@gmail.com>
2023-09-29 16:14:09 +0530

    Initial Commit

--- src/main/kotlin/exception/CathingExceptionExample.kt
+++ src/main/kotlin/exception/CathingExceptionExample.kt
@@ -1,2 +1,25 @@
 package exception
 
+fun main(){
+    try {
+        println("Hello")
+        function1()
+    }catch (e: IlligalNumberException){
+        println(e.message)
+    }finally {
+        println("Finally")
+    }
+
+    //function3()
+}
+
+fun function1(){
+    throw IlligalNumberException("this is the Illigal Number exception")
+}
+fun function2(){
+    function1()
+}
+fun function3(){
+    function2()
+}
+class IlligalNumberException(description:String) : Exception(description)
