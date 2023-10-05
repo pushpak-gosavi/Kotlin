afb6ae171d34b60d3871b12af73c9581df946f01
Pushpak <pushpak.gosavi7@gmail.com>
2023-10-05 11:59:58 +0530

    Initial Commit

---
+++ src/main/kotlin/programs/PrimeNumber.kt
@@ -0,0 +1,24 @@
+package programs
+
+fun main() {
+//    for(i in 0..100) {
+//        checkPrimeNumber(number = i)
+//    }
+    checkPrimeNumber(number = 97)
+}
+fun checkPrimeNumber(number: Int) {
+    var flag = false
+    if (number > 1) {
+        for (i in 2..number / 2) {
+            if (number % i == 0) {
+                flag = true
+                break
+            }
+        }
+        if (!flag)
+            println("$number is Prime Number")
+        else
+            println("$number is not Prime Number")
+    }
+
+}
