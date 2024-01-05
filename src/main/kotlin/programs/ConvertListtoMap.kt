6d3383549d27370295f5743cd0b539bdc64ab930
Pushpak Gosavi <pushpak.gosavi7@gmail.com>
2024-01-05 19:35:09 +0530

    master: associate and withIndex

--- src/main/kotlin/programs/ConvertListtoMap.kt
+++ src/main/kotlin/programs/ConvertListtoMap.kt
@@ -4,8 +4,27 @@ fun main() {
     val stringList: MutableList<String> = mutableListOf("A", "B", "C")
 
     val resultString = stringList.map { alphabets ->
-        mapOf("Name" to alphabets)
+        mapOf("\"Name\"" to alphabets)
     }
 
     println(resultString)
+
+    /** Q2
+     * Given an array of string, such as
+
+    ["A", "B", "C", "Z", "Q"]
+
+    return an object such as
+
+    { "A": 0,  "B": 1,  "C": 2,  "Z" : 3,  "Q": 4 }
+
+    The numbers here are the positions of the string in the input array **/
+
+    val secondAlphabetList = listOf("A", "B", "C", "Z", "Q")
+    val resultSecondList = secondAlphabetList
+        .withIndex()
+        .associate { (index, value) -> "\"$value\"" to index }
+        println(resultSecondList)
 }
+
+
