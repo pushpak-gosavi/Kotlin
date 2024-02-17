836b35114f3f5cbba0cb722f1f99e6d81ca4ceb0
Pushpak Gosavi <pushpak.gosavi7@gmail.com>
2024-02-17 13:14:46 +0530

    master:Stable, swap 0 to last

--- src/main/kotlin/SwaptheElement.kt
+++ src/main/kotlin/SwaptheElement.kt
@@ -1,12 +1,9 @@
 fun main(){
     val intList = mutableListOf(1,2,3,-1,0,-5,0,4)
-    var number = 0
     for(i in intList.indices){
         if(intList[i] == 0){
             intList.removeAt(i)
-            //intList.add(intList[i+1])
             intList.add(0)
-            number++
         }
     }
     println(intList)
