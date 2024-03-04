2241033c8d3fadbaa2ec27a7575719b26bffffcf
Pushpak Gosavi <pushpak.gosavi7@gmail.com>
2024-03-04 16:55:43 +0530

    master:Stable, implement the Lamda expresion

--- src/main/kotlin/LambdaFunction.kt
+++ src/main/kotlin/LambdaFunction.kt
@@ -1,3 +1,6 @@
+import kotlin.random.Random
+import kotlin.random.nextInt
+
 fun main() {
     val listIs = listOf(1, 2, 3)
     val lamdaUse = listIs.map(transform = { number ->
@@ -5,4 +8,28 @@ fun main() {
     }
     )
     println(lamdaUse)
+
+    val sum = {a:Int , b:Int -> a+b}
+    val s = sum(10 ,20)
+    println(s)
+
+    lamdaFun { number ->
+        println("number is $number")
+       "number is $number"
+    }
+
+    noArgumentlamda {
+        println("Empty lamda")
+    }
+
 }
+
+fun lamdaFun(getLamda: (Int) -> String){
+    getLamda(Random.nextInt(1..5))
+}
+
+fun noArgumentlamda(noargs: () -> Unit){
+    noargs()
+}
+
+
